becd5fddaaeedb253ae93782f90ce9460e9bb721
Utkarsh Gaur <charmedtobe@gmail.com>
2024-11-28 10:20:55 -0500

    added rallies counter

--- src/StatCounter.tsx
+++ src/StatCounter.tsx
@@ -1,15 +1,15 @@
 import d20 from './d20.png';
 import './StatCounter.css';
 
-const modifyStat = (stat: number, setterFunction: (stat: number) => void, modification: number) => {
-    setterFunction(stat + modification);
+const modifyStat = (stat: number, setterFunction: (stat: number) => void, modification: number, min: number, max: number) => {
+    setterFunction(Math.max(Math.min(stat + modification, max), min));
 }
 
-export default function StatCounter({stat, statSetter}: {stat: number, statSetter: (stat: number) => void}){
+export default function StatCounter({stat, statSetter, min, max}: {stat: number, statSetter: (stat: number) => void, min: number, max: number}){
     
     return (<>
-        <span><img id="d20" onClick={(event)=>{ modifyStat(stat, statSetter, -1) }} src={d20} alt="d20" /></span>
+        <span><img id="d20" onClick={(event)=>{ modifyStat(stat, statSetter, -1, min, max ) }} src={d20} alt="d20" /></span>
         <span>{stat}</span>
-        <span><img id="d20" onClick={(event)=>{ modifyStat(stat, statSetter, 1) }} src={d20} alt="d20" /></span>
+        <span><img id="d20" onClick={(event)=>{ modifyStat(stat, statSetter, 1, min, max) }} src={d20} alt="d20" /></span>
     </>)
 }
--- src/TopInfo.tsx
+++ src/TopInfo.tsx
@@ -7,6 +7,7 @@ export default function TopInfo (){
     const [moxie, setMoxie] = useState(0);
     const [stress, setStress] = useState(0);
     const [gumption, setGumption] = useState(0);
+    const [rallies, setRallies] = useState(30);
 
     return (<div className="top-info">
         <div className='top-left'>
@@ -24,15 +25,19 @@ export default function TopInfo (){
         <div className='top-right'>
             <div>
                 <label>Stress:</label>
-                <StatCounter stat={stress} statSetter={setStress} />
+                <StatCounter stat={stress} statSetter={setStress} min={-5} max={20} />
             </div>
             <div>
                 <label>Moxie:</label>
-                <StatCounter stat={moxie} statSetter={setMoxie} />
+                <StatCounter stat={moxie} statSetter={setMoxie} min={0} max={10} />
             </div>
             <div>
                 <label>Gumption:</label>
-                <StatCounter stat={gumption} statSetter={setGumption} />
+                <StatCounter stat={gumption} statSetter={setGumption} min={0} max={20} />
+            </div>
+            <div>
+                <label>Rallies:</label>
+                <StatCounter stat={rallies} statSetter={setRallies} min={0} max={30} />
             </div>
         </div>
     </div>)
